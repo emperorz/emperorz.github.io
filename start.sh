5c365e42692368d38786ed0e134a0bf2a8ac83d7
Richard Henderson <richardh@emperorz.com>
2024-10-18 14:32:21 +0100

    fixed for projects
    very draft projects setup

---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+# See https://docs.github.com/en/pages/setting-up-a-github-pages-site-with-jekyll/testing-your-github-pages-site-locally-with-jekyll
+
+bundle exec jekyll serve --livereload
